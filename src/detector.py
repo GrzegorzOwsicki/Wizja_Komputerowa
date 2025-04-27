dad24d23498dafc91a86e7367a73b09695b50748
sw7105 <188427321+sw7105@users.noreply.github.com>
2025-04-27 19:58:34 +0200

    Aktualizacja parametrów

--- src/detector.py
+++ src/detector.py
@@ -5,11 +5,23 @@ import json
 class Parameters:
     def __init__(self):
         # Uwydatnianie obrazu
+        self.gamma = 1
+        self.white_balance = False
+        self.hist_h = False
+        self.hist_s = False
+        self.hist_v = False
         self.blur_gauss_size = 1
         self.blur_gauss_sigma = 0.5
         self.blur_median_size = 1
-        self.hist_s = False
-        self.hist_v = False
+
+        # Progowanie adaptywne
+        self.adaptive_threshold_h = False
+        self.adaptive_threshold_s = False
+        self.adaptive_threshold_v = False
+        self.adaptive_threshold_size = 21
+        self.adaptive_threshold_c = 6
+        self.adaptive_threshold_erode_dilate_size = 3
+        self.adaptive_threshold_erode_dilate_iter = 0
 
         # Kolor skóry
         self.hue_min = 0
@@ -22,10 +34,12 @@ class Parameters:
         self.erode_dilate_iter = 0
 
         # Filtracja wyników
-        self.area_min = 2000
+        self.area_min = 0.1
+        self.area_max = 0.5
         self.aspect_ratio_min = 0.4
         self.aspect_ratio_max = 1
-        self.circularity_min = 0.75
+        self.circularity_min = 0.5
+        self.circularity_max = 1
 
 class DisplayParams:
     def __init__(self):
@@ -45,11 +59,18 @@ class DisplayParams:
 class Detector:
     def __init__(self):
         self.image = None
+        self.imageWidth = 0 # nie używane
+        self.imageHeight = 0 # nie używane
+        self.imageArea = 0 # używane w filterArea
         self.params = Parameters()
         self.display = DisplayParams()
 
     def openImage(self, filename):
         self.image = cv2.imread(filename)
+        height, width, channels = self.image.shape
+        self.imageArea = width * height
+        self.imageWidth = width
+        self.imageHeight = height
 
     def saveParameters(self, filepath):
         with open(filepath, 'w') as file:
@@ -65,59 +86,31 @@ class Detector:
         p = self.params
 
         # Uwydatnianie obrazu
-        image_blur_gauss = self.imageBlurGauss(self.image)
+        image_gamma_corrected = self.imageGamma(self.image)
+        image_balanced = self.imageWhiteBalance(image_gamma_corrected)
+        image_equalized = self.imageEqualize(image_balanced)
+        image_blur_gauss = self.imageBlurGauss(image_equalized)
         image_blur_median = self.imageBlurMedian(image_blur_gauss)
-        image_equalized = self.imageEqualize(image_blur_median)
 
-        image_enhanced = image_equalized
+        image_enhanced = image_blur_median
         image_hsv = cv2.cvtColor(image_enhanced, cv2.COLOR_BGR2HSV)
-        
 
-        # Maskowanie koloru skóry
-        def get_ranges(min_val, max_val, max_range):
-            if min_val <= max_val:
-                # Przedział wewnętrzny
-                return [(min_val, max_val)]
-            else:
-                # Jeżeli minimum i maksimum są na odwrót
-                # Brane są przedziały zewnętrzne
-                return [(0, max_val), (min_val, max_range)]
-
-        hue_ranges = get_ranges(p.hue_min, p.hue_max, 179)
-        sat_ranges = get_ranges(p.saturation_min, p.saturation_max, 255)
-        val_ranges = get_ranges(p.value_min, p.value_max, 255)
-
-        mask = None # Złożenie maski ze wszyskich przedziałów
-        for h_min, h_max in hue_ranges:
-            for s_min, s_max in sat_ranges:
-                for v_min, v_max in val_ranges:
-                    lower = (h_min, s_min, v_min)
-                    upper = (h_max, s_max, v_max)
-                    temp_mask = cv2.inRange(image_hsv, lower, upper)
-                    if mask is None:
-                        mask = temp_mask
-                    else:
-                        mask = cv2.bitwise_or(mask, temp_mask)
-
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (p.erode_dilate_size, p.erode_dilate_size))
-        maskErodeDilate = mask.copy()
-        if p.erode_dilate_iter < 0: # Erode
-            maskErodeDilate = cv2.erode(maskErodeDilate, kernel, iterations=p.erode_dilate_iter*-1)
-        elif p.erode_dilate_iter > 0: # Dilate
-            maskErodeDilate = cv2.dilate(maskErodeDilate, kernel, iterations=p.erode_dilate_iter)
-
-                
-        image_to_mask = image_enhanced if self.display.mask_enhanced else self.image
-        skin = cv2.bitwise_and(image_to_mask, image_to_mask, mask=maskErodeDilate)
+        # Maskowanie
+        mask_adaptiveThreshold = self.maskAdaptiveThreshold(image_hsv)
+        mask_skinColor = self.maskSkinColor(image_hsv)
+        mask = cv2.bitwise_and(mask_skinColor, mask_adaptiveThreshold)
+     
+        display_image = image_enhanced if self.display.mask_enhanced else self.image
+        skin = cv2.bitwise_and(display_image, display_image, mask=mask)
 
         # Detekcja konturów
-        contours, hierarchy = cv2.findContours(maskErodeDilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contours = list(contours)
 
         def get_contours_mask_skin(contours):
             newMask = np.zeros_like(mask, dtype=np.uint8)
             cv2.drawContours(newMask, contours, -1, 255, thickness=cv2.FILLED)
-            newSkin = cv2.bitwise_and(image_to_mask, image_to_mask, mask=newMask)
+            newSkin = cv2.bitwise_and(display_image, display_image, mask=newMask)
             return newMask, newSkin
 
         # Filtracja Wyników
@@ -128,20 +121,18 @@ class Detector:
         maskFilterAspectRatio, skinFilterAspectRatio = get_contours_mask_skin(contours)
         contours = [cnt for cnt in contours if self.filterCircularity(cnt, mask)] # To samo tylko zapisane inaczej aby dodać dodatkowy parametr mask
         maskFilterCircularity, skinFilterCircularity = get_contours_mask_skin(contours)
+        
+        skinFiltered = skinFilterCircularity
 
         # Wyświetlanie wyniku
         self.displayResult(contours, 
             [
-                self.image,
-                image_blur_gauss, 
-                image_blur_median,
-                image_equalized,
-                mask, 
-                maskErodeDilate,
+                display_image,
+                mask_adaptiveThreshold,
+                mask_skinColor, 
+                mask,
                 skin, 
-                skinFilterArea, 
-                skinFilterAspectRatio, 
-                skinFilterCircularity
+                skinFiltered
             ])
 
         return contours
@@ -170,6 +161,109 @@ class Detector:
         cv2.namedWindow("Detekcja Twarzy", cv2.WINDOW_KEEPRATIO)
         cv2.imshow("Detekcja Twarzy", display)
 
+    def maskAdaptiveThreshold(self, image_hsv):
+        p = self.params
+        image_h, image_s, image_v = cv2.split(image_hsv)
+        mask_h = np.ones_like(image_h, dtype=np.uint8) * 255
+        mask_s = mask_h.copy()
+        mask_v = mask_h.copy()
+        if p.adaptive_threshold_h:
+            mask_h = cv2.adaptiveThreshold(image_h, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, p.adaptive_threshold_size, p.adaptive_threshold_c)
+        if p.adaptive_threshold_s:
+            mask_s = cv2.adaptiveThreshold(image_s, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, p.adaptive_threshold_size, p.adaptive_threshold_c)
+        if p.adaptive_threshold_v:
+            mask_v = cv2.adaptiveThreshold(image_v, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, p.adaptive_threshold_size, p.adaptive_threshold_c)
+        mask = cv2.bitwise_and(mask_h, mask_s, mask=mask_v)
+
+        # Erozja - Dylatacja
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (p.adaptive_threshold_erode_dilate_size, p.adaptive_threshold_erode_dilate_size))
+        maskErodeDilate = mask.copy()
+        if p.adaptive_threshold_erode_dilate_iter < 0: # Erode
+            maskErodeDilate = cv2.erode(maskErodeDilate, kernel, iterations=p.adaptive_threshold_erode_dilate_iter*-1)
+        elif p.adaptive_threshold_erode_dilate_iter > 0: # Dilate
+            maskErodeDilate = cv2.dilate(maskErodeDilate, kernel, iterations=p.adaptive_threshold_erode_dilate_iter)
+
+        return maskErodeDilate
+        
+
+    def maskSkinColor(self, image_hsv):
+        p = self.params
+        def get_ranges(min_val, max_val, max_range):
+            if min_val <= max_val:
+                # Przedział wewnętrzny
+                return [(min_val, max_val)]
+            else:
+                # Jeżeli minimum i maksimum są na odwrót
+                # Brane są przedziały zewnętrzne
+                return [(0, max_val), (min_val, max_range)]
+
+        hue_ranges = get_ranges(p.hue_min, p.hue_max, 179)
+        sat_ranges = get_ranges(p.saturation_min, p.saturation_max, 255)
+        val_ranges = get_ranges(p.value_min, p.value_max, 255)
+
+        mask = None # Złożenie maski ze wszyskich przedziałów
+        for h_min, h_max in hue_ranges:
+            for s_min, s_max in sat_ranges:
+                for v_min, v_max in val_ranges:
+                    lower = (h_min, s_min, v_min)
+                    upper = (h_max, s_max, v_max)
+                    temp_mask = cv2.inRange(image_hsv, lower, upper)
+                    if mask is None:
+                        mask = temp_mask
+                    else:
+                        mask = cv2.bitwise_or(mask, temp_mask)
+
+        # Erozja - Dylatacja
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (p.erode_dilate_size, p.erode_dilate_size))
+        maskErodeDilate = mask.copy()
+        if p.erode_dilate_iter < 0: # Erode
+            maskErodeDilate = cv2.erode(maskErodeDilate, kernel, iterations=p.erode_dilate_iter*-1)
+        elif p.erode_dilate_iter > 0: # Dilate
+            maskErodeDilate = cv2.dilate(maskErodeDilate, kernel, iterations=p.erode_dilate_iter)
+
+        return maskErodeDilate
+
+    def imageWhiteBalance(self, image):
+        """
+        Perform white balance using Gray World Assumption.
+        """
+        p = self.params
+        if p.white_balance == False:
+            return image
+        result = image.copy().astype(np.float32)
+
+        avg_b = np.mean(result[:, :, 0])
+        avg_g = np.mean(result[:, :, 1])
+        avg_r = np.mean(result[:, :, 2])
+
+        avg_gray = (avg_b + avg_g + avg_r) / 3
+
+        # Scale each channel so that the avg becomes avg_gray
+        result[:, :, 0] *= (avg_gray / avg_b)
+        result[:, :, 1] *= (avg_gray / avg_g)
+        result[:, :, 2] *= (avg_gray / avg_r)
+
+        # Clip to [0, 255] and convert back
+        result = np.clip(result, 0, 255).astype(np.uint8)
+        return result
+
+    def imageEqualize(self, image):
+        p = self.params
+        image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        h, s, v = cv2.split(image_hsv)
+        h_eq = cv2.equalizeHist(h) if p.hist_h else h
+        s_eq = cv2.equalizeHist(s) if p.hist_s else s
+        v_eq = cv2.equalizeHist(v) if p.hist_v else v
+        image_hsv = cv2.merge((h_eq, s_eq, v_eq))
+        image_bgr = cv2.cvtColor(image_hsv, cv2.COLOR_HSV2BGR)
+        return image_bgr
+
+    def imageGamma(self, image):
+        normalized_image = image / 255.0
+        corrected_image = np.power(normalized_image, self.params.gamma)
+        corrected_image = np.uint8(corrected_image * 255)
+        return corrected_image
+
     def imageBlurGauss(self, image):
         p = self.params
         if p.blur_gauss_size < 2:
@@ -186,21 +280,22 @@ class Detector:
             p.blur_median_size += 1
         return cv2.medianBlur(image, p.blur_median_size)
 
-    def imageEqualize(self, image):
-        p = self.params
-        image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        h, s, v = cv2.split(image_hsv)
-        s_eq = cv2.equalizeHist(s) if p.hist_s else s
-        v_eq = cv2.equalizeHist(v) if p.hist_v else v
-        image_hsv = cv2.merge((h, s_eq, v_eq))
-        image_bgr = cv2.cvtColor(image_hsv, cv2.COLOR_HSV2BGR)
-        return image_bgr
-
     def filterCanFitElipse(self, contour):
         return len(contour) >= 5 # cv2.fitEllipse wymaga minimum 5 punktów w konturze
 
     def filterArea(self, contour):
-        return cv2.contourArea(contour) > self.params.area_min
+        # POLE KONTURU A NIE KWADRATU
+        area = cv2.contourArea(contour)
+        areaPct = area / self.imageArea
+
+        # Zauważyłem że pola często są w bardzo małych wartościach typu 0.05
+        # Dlatego aby uprościć parametr w gui, zostaje tutaj zmieniona jego skala
+        # Jako skutek uboczny parametr już nie odzwierciedla % powierzchni
+        # Tutaj robimy skalę pierwiastek o podstawie 2, polecam sobie zobaczyć na wykresie jak to wygląda
+        # https://www.desmos.com/calculator/r0hnoridtu
+        areaPct = areaPct ** 0.5 
+
+        return self.params.area_min <= areaPct <= self.params.area_max
 
     def filterAspectRatio(self, contour):
         # Obliczamy proporcje za pomocą elips bo uwzględniane są pod każdym kątem obrotu
@@ -213,9 +308,9 @@ class Detector:
         
         p = self.params
         if (p.aspect_ratio_min > p.aspect_ratio_max):
-            return aspect_ratio < p.aspect_ratio_max or aspect_ratio > p.aspect_ratio_min
+            return aspect_ratio <= p.aspect_ratio_max or aspect_ratio >= p.aspect_ratio_min
 
-        return p.aspect_ratio_min < aspect_ratio < p.aspect_ratio_max
+        return p.aspect_ratio_min <= aspect_ratio <= p.aspect_ratio_max
 
     def filterCircularity(self, contour, mask):
         ellipse = cv2.fitEllipse(contour)
@@ -232,17 +327,12 @@ class Detector:
         overlap = cv2.bitwise_and(contour_region, ellipse_mask)
         overlap_pixels = cv2.countNonZero(overlap)
 
-        # Część elipsy poza konturem
-        spill1 = cv2.bitwise_and(cv2.bitwise_not(contour_region), ellipse_mask)
-        spill1_pixels = cv2.countNonZero(spill1)
+        # Część wspólna i różna
+        total = cv2.bitwise_or(contour_region, ellipse_mask)
+        total_pixels = cv2.countNonZero(total)
 
-        # Część konturu poza elipsą
-        spill2 = cv2.bitwise_and(contour_region, cv2.bitwise_not(ellipse_mask))
-        spill2_pixels = cv2.countNonZero(spill2)
-
-        total_evaluated = overlap_pixels + spill1_pixels + spill2_pixels
-        if total_evaluated == 0:
+        if total_pixels == 0:
             return False
 
-        circularity = overlap_pixels / total_evaluated
-        return circularity >= self.params.circularity_min
+        circularity = overlap_pixels / total_pixels
+        return self.params.circularity_min <= circularity <= self.params.circularity_max 
--- src/gui.py
+++ src/gui.py
@@ -6,6 +6,7 @@ from detector import Detector
 class GUI:
     def __init__(self, master):
         self.detector = Detector()
+        self.display_stages = ["Obraz", "Progowanie adaptywne (maska1)", "Kolor skóry (maska2)", "maska1 AND maska2", "Wykryta skóra", "Odfiltrowane twarze"]
 
         self.master = master
         self.master.title("Parametry")
@@ -28,21 +29,58 @@ class GUI:
         self.image_tab = ttk.Frame(self.tab_control)
         self.tab_control.add(self.image_tab, text='Uwydatnianie obrazu')
 
+        self.white_balance_var = tk.BooleanVar(value=False)
+
+        self.hist_h_var = tk.BooleanVar(value=False)
+        self.hist_s_var = tk.BooleanVar(value=False)
+        self.hist_v_var = tk.BooleanVar(value=False)
+
+        self.gamma = tk.Scale(self.image_tab, from_=0, to=5, resolution=0.01, orient='horizontal', command=self.update_params, label="Gamma")
+        self.gamma.pack(fill='x')
+
+        self.white_balance_check =tk.Checkbutton(self.image_tab, text="Balans bieli", command=self.update_params, variable=self.white_balance_var)
+        self.white_balance_check.pack(anchor='w')
+
+        self.hist_label = tk.Label(self.image_tab, text="Wyrównaj histogram").pack(anchor='w')
+        self.hist_h_check =tk.Checkbutton(self.image_tab, text="Hue", command=self.update_params, variable=self.hist_h_var)
+        self.hist_h_check.pack(anchor='w')
+        self.hist_s_check =tk.Checkbutton(self.image_tab, text="Saturation", command=self.update_params, variable=self.hist_s_var)
+        self.hist_s_check.pack(anchor='w')
+        self.hist_v_check =tk.Checkbutton(self.image_tab, text="Value", command=self.update_params, variable=self.hist_v_var)
+        self.hist_v_check.pack(anchor='w')
+
         self.blur_gauss_size = tk.Scale(self.image_tab, from_=1, to=31, orient='horizontal', command=self.update_params, label="Rozmycie Gaussa (rozmiar, sigma)")
-        self.blur_gauss_sigma = tk.Scale(self.image_tab, from_=0, to=32, resolution=0.1, orient='horizontal', command=self.update_params)
+        self.blur_gauss_sigma = tk.Scale(self.image_tab, from_=0, to=32, resolution=0.01, orient='horizontal', command=self.update_params)
         self.blur_gauss_size.pack(fill='x')
         self.blur_gauss_sigma.pack(fill='x')
 
         self.blur_median_size = tk.Scale(self.image_tab, from_=1, to=31, orient='horizontal', command=self.update_params, label="Rozmycie medianowe (rozmiar)")  
         self.blur_median_size.pack(fill='x')
 
-        self.hist_s_var = tk.BooleanVar(value=False)
-        self.hist_v_var = tk.BooleanVar(value=False)
+        # Progowanie adaptywne
+        self.adaptiveThreshold_tab = ttk.Frame(self.tab_control)
+        self.tab_control.add(self.adaptiveThreshold_tab, text='Progowanie adaptywne')
 
-        self.hist_s_check =tk.Checkbutton(self.image_tab, text="Wyrównaj histogram Saturation", command=self.update_params, variable=self.hist_s_var)
-        self.hist_s_check.pack(anchor='w')
-        self.hist_v_check =tk.Checkbutton(self.image_tab, text="Wyrównaj histogram Value", command=self.update_params, variable=self.hist_v_var)
-        self.hist_v_check.pack(anchor='w')
+        self.adaptive_threshold_h = tk.BooleanVar(value=False)
+        self.adaptive_threshold_s = tk.BooleanVar(value=False)
+        self.adaptive_threshold_v = tk.BooleanVar(value=False)
+
+        self.adaptive_threshold_size = tk.Scale(self.adaptiveThreshold_tab, from_=3, to=63, orient='horizontal', command=self.update_params, label="Progowanie Adaptywne (rozmiar, stała C)")
+        self.adaptive_threshold_size.pack(fill='x')
+        self.adaptive_threshold_c = tk.Scale(self.adaptiveThreshold_tab, from_=-32, to=32, orient='horizontal', command=self.update_params)
+        self.adaptive_threshold_c.pack(fill='x')
+
+        self.adaptive_threshold_h_check =tk.Checkbutton(self.adaptiveThreshold_tab, text="Hue", command=self.update_params, variable=self.adaptive_threshold_h)
+        self.adaptive_threshold_h_check.pack(anchor='w')
+        self.adaptive_threshold_s_check =tk.Checkbutton(self.adaptiveThreshold_tab, text="Saturation", command=self.update_params, variable=self.adaptive_threshold_s)
+        self.adaptive_threshold_s_check.pack(anchor='w')
+        self.adaptive_threshold_v_check =tk.Checkbutton(self.adaptiveThreshold_tab, text="Value", command=self.update_params, variable=self.adaptive_threshold_v)
+        self.adaptive_threshold_v_check.pack(anchor='w')
+
+        self.adaptive_threshold_erode_dilate_size = tk.Scale(self.adaptiveThreshold_tab, from_=3, to=31, orient='horizontal', command=self.update_params, label="Erozja - Dylatacja (rozmiar - iteracje)")
+        self.adaptive_threshold_erode_dilate_iter = tk.Scale(self.adaptiveThreshold_tab, from_=-16, to=16, orient='horizontal', command=self.update_params)
+        self.adaptive_threshold_erode_dilate_size.pack(fill='x')
+        self.adaptive_threshold_erode_dilate_iter.pack(fill='x')
 
         # Kolor skóry
         self.skin_tab = ttk.Frame(self.tab_control)
@@ -72,22 +110,26 @@ class GUI:
         self.filter_tab = ttk.Frame(self.tab_control)
         self.tab_control.add(self.filter_tab, text='Filtrowanie')
 
-        self.area_min = tk.Scale(self.filter_tab, from_=8, to=18, resolution=0.01, orient='horizontal',command=self.update_params, label="Pole (min)")
+        self.area_min = tk.Scale(self.filter_tab, from_=0, to=1, resolution=0.01, orient='horizontal',command=self.update_params, label="Pole KONTURU (min - max)")
         self.area_min.pack(fill='x')
+        self.area_max = tk.Scale(self.filter_tab, from_=0, to=1, resolution=0.01, orient='horizontal',command=self.update_params)
+        self.area_max.pack(fill='x')
 
         self.aspect_ratio_min = tk.Scale(self.filter_tab, from_=0, to=1, resolution=0.01, orient='horizontal', command=self.update_params, label="Proporcje (min - max) 1 = kwadrat")
         self.aspect_ratio_max = tk.Scale(self.filter_tab, from_=0, to=1, resolution=0.01, orient='horizontal', command=self.update_params)
         self.aspect_ratio_min.pack(fill='x')
         self.aspect_ratio_max.pack(fill='x')
 
-        self.circularity_min = tk.Scale(self.filter_tab, from_=0, to=1, resolution=0.01, orient='horizontal', command=self.update_params, label="Okrągłość (min)")
+        self.circularity_min = tk.Scale(self.filter_tab, from_=0, to=1, resolution=0.01, orient='horizontal', command=self.update_params, label="Okrągłość (min - max)")
         self.circularity_min.pack(fill='x')
+        self.circularity_max = tk.Scale(self.filter_tab, from_=0, to=1, resolution=0.01, orient='horizontal', command=self.update_params)
+        self.circularity_max.pack(fill='x')
 
         # Wyświetlanie
         self.display_tab = ttk.Frame(self.tab_control)
         self.tab_control.add(self.display_tab, text='Wyświetlanie')
 
-        self.display_stage = tk.Scale(self.display_tab, label="Faza przetwarzania", from_=0, to=9, orient='horizontal', command=self.update_params)
+        self.display_stage = tk.Scale(self.display_tab, label="Faza przetwarzania", from_=0, to=5, orient='horizontal', command=self.update_params)
         self.display_stage.pack(fill='x')
 
         self.contour_var = tk.BooleanVar(value=False)
@@ -101,7 +143,7 @@ class GUI:
         self.rectangle_check.pack(anchor='w')
         self.ellipse_check = tk.Checkbutton(self.display_tab, text="Elipsy", command=self.update_params, variable=self.ellipse_var)
         self.ellipse_check.pack(anchor='w')
-        self.mask_enhanced_check = tk.Checkbutton(self.display_tab, text="Maski na obrazie uwydatnionym", command=self.update_params, variable=self.mask_enhanced_var)
+        self.mask_enhanced_check = tk.Checkbutton(self.display_tab, text="Obraz uwydatniony", command=self.update_params, variable=self.mask_enhanced_var)
         self.mask_enhanced_check.pack(anchor='w', pady=(5,0))
 
         # Pack the tab control
@@ -113,11 +155,22 @@ class GUI:
     def init_params(self):
         p = self.detector.params
 
+        self.gamma.set(p.gamma)
+        self.white_balance_var.set(p.white_balance)
+        self.hist_h_var.set(p.hist_h)
+        self.hist_s_var.set(p.hist_s)
+        self.hist_v_var.set(p.hist_v)
         self.blur_gauss_size.set(p.blur_gauss_size)
         self.blur_gauss_sigma.set(p.blur_gauss_sigma)
         self.blur_median_size.set(p.blur_median_size)
-        self.hist_s_var.set(p.hist_s)
-        self.hist_v_var.set(p.hist_v)
+
+        self.adaptive_threshold_h.set(p.adaptive_threshold_h)
+        self.adaptive_threshold_s.set(p.adaptive_threshold_s)
+        self.adaptive_threshold_v.set(p.adaptive_threshold_v)
+        self.adaptive_threshold_size.set(p.adaptive_threshold_size)
+        self.adaptive_threshold_c.set(p.adaptive_threshold_c)
+        self.adaptive_threshold_erode_dilate_size.set(p.adaptive_threshold_erode_dilate_size)
+        self.adaptive_threshold_erode_dilate_iter.set(p.adaptive_threshold_erode_dilate_iter)
 
         self.hue_min.set(p.hue_min)
         self.hue_max.set(p.hue_max)
@@ -128,12 +181,15 @@ class GUI:
         self.erode_dilate_size.set(p.erode_dilate_size)
         self.erode_dilate_iter.set(p.erode_dilate_iter)
 
-        self.area_min.set(math.log2(p.area_min))
+        self.area_min.set(p.area_min)
+        self.area_max.set(p.area_max)
         self.aspect_ratio_min.set(p.aspect_ratio_min)
         self.aspect_ratio_max.set(p.aspect_ratio_max)
         self.circularity_min.set(p.circularity_min)
+        self.circularity_max.set(p.circularity_max)
 
         pd = self.detector.display
+        self.display_stage.config(label="Faza przetwarzania:    "+self.display_stages[pd.stage])
         self.display_stage.set(pd.stage)
         self.contour_var.set(pd.contours)
         self.rectangle_var.set(pd.rectangles)
@@ -141,6 +197,11 @@ class GUI:
         self.mask_enhanced_var.set(pd.mask_enhanced)
 
     def update_params(self, event=None):
+        self.detector.params.gamma = self.gamma.get()
+        self.detector.params.white_balance = self.white_balance_var.get()
+        self.detector.params.hist_h = self.hist_h_var.get()
+        self.detector.params.hist_s = self.hist_s_var.get()
+        self.detector.params.hist_v = self.hist_v_var.get()
         if self.blur_gauss_size.get() % 2 == 0:
             self.blur_gauss_size.set(self.blur_gauss_size.get()+1)
         self.detector.params.blur_gauss_size = self.blur_gauss_size.get()
@@ -148,8 +209,18 @@ class GUI:
         if self.blur_median_size.get() % 2 == 0:
             self.blur_median_size.set(self.blur_median_size.get()+1)
         self.detector.params.blur_median_size = self.blur_median_size.get()
-        self.detector.params.hist_s = self.hist_s_var.get()
-        self.detector.params.hist_v = self.hist_v_var.get()
+
+        self.detector.params.adaptive_threshold_h = self.adaptive_threshold_h.get()
+        self.detector.params.adaptive_threshold_s = self.adaptive_threshold_s.get()
+        self.detector.params.adaptive_threshold_v = self.adaptive_threshold_v.get()
+        if self.adaptive_threshold_size.get() % 2 == 0:
+            self.adaptive_threshold_size.set(self.adaptive_threshold_size.get()+1)
+        self.detector.params.adaptive_threshold_size = self.adaptive_threshold_size.get()
+        self.detector.params.adaptive_threshold_c = self.adaptive_threshold_c.get()
+        if self.adaptive_threshold_erode_dilate_size.get() % 2 == 0:
+            self.adaptive_threshold_erode_dilate_size.set(self.adaptive_threshold_erode_dilate_size.get()+1)
+        self.detector.params.adaptive_threshold_erode_dilate_size = self.adaptive_threshold_erode_dilate_size.get()
+        self.detector.params.adaptive_threshold_erode_dilate_iter = self.adaptive_threshold_erode_dilate_iter.get()
 
         self.detector.params.hue_min = self.hue_min.get()
         self.detector.params.hue_max = self.hue_max.get()
@@ -162,15 +233,15 @@ class GUI:
         self.detector.params.erode_dilate_size = self.erode_dilate_size.get()
         self.detector.params.erode_dilate_iter = self.erode_dilate_iter.get()
 
-        area_min_val = self.area_min.get()
-        area_min_val = int(2 ** area_min_val)
-        self.area_min.config(label=f"Pole (min {area_min_val}px)")
-        self.detector.params.area_min = area_min_val
+        self.detector.params.area_min = self.area_min.get()
+        self.detector.params.area_max = self.area_max.get()
         self.detector.params.aspect_ratio_min = self.aspect_ratio_min.get()
         self.detector.params.aspect_ratio_max = self.aspect_ratio_max.get()
         self.detector.params.circularity_min = self.circularity_min.get()
+        self.detector.params.circularity_max = self.circularity_max.get()
 
         self.detector.display.stage = self.display_stage.get()
+        self.display_stage.config(label="Faza przetwarzania:    "+self.display_stages[self.display_stage.get()])
         self.detector.display.contours = self.contour_var.get()
         self.detector.display.rectangles = self.rectangle_var.get()
         self.detector.display.ellipses = self.ellipse_var.get()
